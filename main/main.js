1da04555a923da54d4e620973858a5b71193132b
monkey214207 <840972208@qq.com>
2019-03-04 20:59:44 +0800

    Changes to be committed: 	modified:   main/main.js 	modified:   spec/main-spec.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(distance,time) {
+    var base = 6;
+    var shortbase = (8-2)*0.8;
+    var timeprice = 0.25*time;
+    var price = 0;
+    if(distance <= 2){
+        price = base;
+    }
+    else if(distance <= 8){
+        price = base+0.8*(distance-2);
+    }
+    else{
+        price = base+shortbase+(distance-8)*0.8*1.5;
+    }
+    return Math.round(price+timeprice);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,38 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it("1 km without waiting", function(){
+        let distance = 1;
+        let time = 0;
+        let result = main(distance,time);
+        expect(result).toBe(6);
+    });
+
+    it("7 km without waiting", function(){
+        let distance = 7;
+        let time = 0;
+        let result = main(distance,time);
+        expect(result).toBe(10);
+    });
+
+    it("9 km without waiting", function(){
+        let distance = 9;
+        let time = 0;
+        let result = main(distance,time);
+        expect(result).toBe(12);
+    });
+
+    it("9 km with 1 min waiting", function(){
+        let distance = 9;
+        let time = 4;
+        let result = main(distance,time);
+        expect(result).toBe(13);
+    });
+
+    it("9 km with 1 min waiting(need round)", function(){
+        let distance = 9;
+        let time = 1;
+        let result = main(distance,time);
+        expect(result).toBe(12);
+    });
 });
